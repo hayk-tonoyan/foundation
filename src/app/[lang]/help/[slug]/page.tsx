af7e9bdfe400ffe138d414a1daafd2be41a24dc0
Hayk Tonoyan <hayk.t@ucraft.com>
2025-01-29 12:44:45 +0400

    fix

--- src/app/[lang]/help/[slug]/page.tsx
+++ src/app/[lang]/help/[slug]/page.tsx
@@ -3,8 +3,17 @@ import { notFound } from "next/navigation";
 import InnerPage from "@/appPages/Help/InnerPage";
 import { helpData } from "@/common/constants";
 
-export default async function HelpDetailPage({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+interface IProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function HelpDetailPage(props: IProps) {
+  const params = await props.params;
+
+  const {
+    slug
+  } = params;
+
   const decodedSlug = decodeURIComponent(slug);
 
   const item = helpData?.find((data) => data.title.replace(/ /g, '-') === decodedSlug);
