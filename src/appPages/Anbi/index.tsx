bf41a08292792d8b8aa0836227b896bab5e582eb
Hayk Tonoyan <hayk.t@ucraft.com>
2025-01-31 16:25:52 +0400

    final

--- src/appPages/Anbi/index.tsx
+++ src/appPages/Anbi/index.tsx
@@ -1,74 +1,57 @@
 'use client';
 
-// import React, { useState } from 'react';
-// // @ts-ignore
-// import { Worker, Viewer } from 'react-pdf-viewer';
+import {useTranslations} from "next-intl";
+
+import './index.css';
 
 export default function Anbi() {
-  // const [pageIndex, setPageIndex] = useState(0);
+  const t = useTranslations();
 
   return (
     <div className="anbi-page-holder page">
       <h2 className="page-title ellipsis">ANBI</h2>
       <div className="anbi-page">
         <div className="anbi-content">
-          <p><strong>KVK-nummer: </strong>92743234</p>
-          <p><strong>RSIN: </strong>866158959</p>
-          <p><strong>Rekeningnummer: </strong>NL92INGB0108325733</p>
-          <p><strong>Postadres: </strong>Smaragd 123, 5231 KH ‘s-Hertogenbosch</p>
-          <p><strong>Telefoon: </strong>+31651852165</p>
-          <p><strong>E-mail: </strong>info@hay-network.com</p>
-          <p>
-            <strong>Missie</strong>
-            <br/>
-            Stichting Hay Network is een organisatie die zich inzet voor de ondersteuning en verbetering van het welzijn van de Armeense diaspora en hulpbehoevenden met Armeense wortels.
-          </p>
-          <div>
-            <p><strong>Doelstellingen</strong></p>
+          <div className="sub-title-holder">
+            <p className="sub-title">{t('mission')}</p>
+            <p className="sub-title-desc">{t('compensation_policy_and_reimbursements')}</p>
+          </div>
+          <div className="list-view-desc">
+            <p><strong>{t('goals.title')}</strong></p>
             <ul>
-              <li>Het bieden van ondersteuning bij onderwijs voor de Armeense diaspora.</li>
-              <li>Het verlenen van humanitaire hulp aan hulpbehoevenden met Armeense wortels.</li>
-              <li>Het bieden van hulp aan weeshuizen, ouderen en mensen met een handicap.</li>
-              <li>Het creëren van samenwerkingsverbanden, alsmede het versterken en stimuleren van Nederlandse organisaties, bedrijven en overheden voor projecten, alles in de ruimste zin van het woord.</li>
+              {[...new Array(4)].map((_, i) => (
+                <li key={i}>{t(`goals.${i + 1}`)}</li>
+              ))}
             </ul>
           </div>
-          <div>
-            <p><strong>Hoofddoelen beleidsplan</strong></p>
+          <div className="list-view-desc">
+            <p><strong>{t('main_goals_policy_plan.title')}</strong></p>
             <ul>
-              <li>Het verzoeken van onderwijsinstellingen om ondersteuning te bieden, onder andere in benodigdheden (materialen), en het geven van advies.</li>
-              <li>Het vragen van (materiële) hulp aan bedrijven, (overheid)instanties en individuen.</li>
-              <li>Het organiseren van bijeenkomsten om geld en/of goederen in te zamelen.</li>
-              <li>Het vragen van ziekenhuizen en andere gezondheidsinstellingen om (afgeschreven) materialen te doneren.</li>
-              <li>Het zoeken naar stageplekken en -opdrachten en online banen voor afgestudeerden, alles in de ruimste zin van het woord.</li>
+              {[...new Array(5)].map((_, i) => (
+                <li key={i}>{t(`main_goals_policy_plan.${i + 1}`)}</li>
+              ))}
             </ul>
           </div>
-          <div>
-            <p><strong>Samenstelling van het bestuur:</strong></p>
-            <p>
-              <span>Voorzitter:</span>
-              <span>T. Pogosian</span>
-            </p>
-            <p>
-              <span>Secretaris:</span>
-              <span>M. Yildiz</span>
-            </p>
-            <p>
-              <span>Penningmeester:</span>
-              <span>G. Akopian</span>
-            </p>
+          <div className="information-desc">
+            <p>{t('kvk_number')}</p>
+            <p>{t('rsin')}</p>
+            <p>{t('account_number')}</p>
+            <p>{t('postal_address')}</p>
+            <p>{t('phone')}</p>
+            <p>{t('email')}</p>
           </div>
-          <p>
-            <strong>Beloningsbeleid en vergoedingen</strong>
-            <br/>
-            Buiten de vergoeding van de door hen gemaakte kosten, ontvangen de bestuursleden en vrijwilligers geen beloning voor hun diensten aan de stichting.
-          </p>
         </div>
-        <div className="pdf-viwers">
-          {/*<div style={{flex: 1, margin: '0 10px'}}>*/}
-          {/*  <Viewer*/}
-          {/*    fileUrl="/pdf1.pdf"*/}
-          {/*  />*/}
-          {/*</div>*/}
+        <div className="pdf-viewers">
+          <div className="pdf-container">
+            <h4>Hay Network Jaarrekening 2024.pdf</h4>
+            <iframe title="pdf1"
+                    src="https://drive.google.com/file/d/1zB3ZDaWf3qjakmHnqe3b_1ApzzFv3fTw/preview"></iframe>
+          </div>
+
+          <div className="pdf-container">
+            <h4>Beleidsplan Hay Network.pdf</h4>
+            <iframe title="pdf2" src="https://drive.google.com/file/d/15-nSsnDx3eugGg22hag7ZrMHWL3tgTlk/preview"></iframe>
+          </div>
         </div>
       </div>
     </div>
